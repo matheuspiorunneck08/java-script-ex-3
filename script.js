c4710392d986f5e9c06f176b5751434240d89662
Laboratório FIAP <matheus.piorunneck08@gmail.com>
2026-03-25 09:46:08 -0300

    ex 3 javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+let texto = prompt('digite seu texto: ');
+let numero = prompt('digite seu numero: ');
+
+let conjunto  = texto + numero;
+
+console.log(`o conjunto de dados é: ${conjunto}`);
+
+alert(`o conjunto é ${conjunto}`);
